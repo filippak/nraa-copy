9ff1e13cf2de71e1ca8d216f528454cb425b6463
fkarrfelt <fkarrfelt@gmail.com>
2018-04-13 08:52:06 +0200

    Test page that fetches data from API

--- TestStructure/TestRekit/src/common/rootReducer.js
+++ TestStructure/TestRekit/src/common/rootReducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import homeReducer from '../features/home/redux/reducer';
 import commonReducer from '../features/common/redux/reducer';
+import fromApiReducer from '../features/from-api/redux/reducer';
+import demoReducer from '../features/demo/redux/reducer';
 
 // NOTE 1: DO NOT CHANGE the 'reducerMap' name and the declaration pattern.
 // This is used for Rekit cmds to register new features, remove features, etc.
@@ -12,6 +14,8 @@ const reducerMap = {
   router: routerReducer,
   home: homeReducer,
   common: commonReducer,
+  fromApi: fromApiReducer,
+  demo: demoReducer,
 };
 
 export default combineReducers(reducerMap);
--- TestStructure/TestRekit/src/common/routeConfig.js
+++ TestStructure/TestRekit/src/common/routeConfig.js
@@ -2,12 +2,16 @@ import { App } from '../features/home';
 import { PageNotFound } from '../features/common';
 import homeRoute from '../features/home/route';
 import commonRoute from '../features/common/route';
+import fromApiRoute from '../features/from-api/route';
+import demoRoute from '../features/demo/route';
 
 // NOTE: DO NOT CHANGE the 'childRoutes' name and the declaration pattern.
 // This is used for Rekit cmds to register routes config for new features, and remove config when remove features, etc.
 const childRoutes = [
   homeRoute,
   commonRoute,
+  fromApiRoute,
+  demoRoute,
 ];
 
 const routes = [{
