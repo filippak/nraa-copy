ee4d251dcb70a3a6e1e7ed69de317ef39d87b084
Amundsson <eric.amundsson@capgemini.com>
2018-04-16 09:50:25 +0200

    Changed the example

--- API/Nra_RestAPI/service/DefaultService.js
+++ API/Nra_RestAPI/service/DefaultService.js
@@ -11,13 +11,13 @@ exports.findRecipients = function(name) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-  "firstname" : "firstname",
+  "firstname" : "Eric",
   "delivery_point_id" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91",
   "starting_date" : "2000-01-23T04:56:07.000+00:00",
-  "expiration_date" : "2000-01-23T04:56:07.000+00:00",
+  "expiration_date" : "2019-01-23T04:56:07.000+00:00",
   "object_id" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91",
   "private_recipient" : true,
-  "lastname" : "lastname"
+  "lastname" : "Amundsson"
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
